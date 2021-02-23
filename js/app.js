7eb1f8235db1b953e17b733651bfc27aa3b81f81
Samuel <samuel.colin@live.fr>
2021-02-23 10:45:28 +0100

    Modification

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ let button = document.getElementById('button');
 button.onclick = function(element) {
   let id = document.getElementById('id').value;
   
-  let action_url = 'https://redmine.senoe.com/redmine/issues/'+id;
+  let action_url = 'https://redmine.com/redmine/issues/'+id;
 
   chrome.tabs.create({url: action_url});
 };
